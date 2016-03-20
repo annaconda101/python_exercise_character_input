1646fe5d01f43a2b5ce3faf41ba942c892fab7ed
anna <annateresavillanueva@gmail.com>
2016-03-20 13:08:13 +1100

    added text_input as an argument for StringChallenge instance, added text_input as a parameter to the String Challenge constructor

--- char_input/char_input.py
+++ char_input/char_input.py
@@ -1,6 +1,9 @@
 class StringChallenge:
-    def get_reversed_string(self, text_input):
-        text_input_list = text_input.split(' ')
+    def __init__(self, text_input):
+        self.text_input = text_input 
+
+    def get_reversed_string(self):
+        text_input_list = self.text_input.split(' ')
         return ' '.join(text_input_list[::-1])
 
 
--- test/test_char_input.py
+++ test/test_char_input.py
@@ -5,6 +5,6 @@ from char_input.char_input import StringChallenge
 class TestStringChallenge(unittest.TestCase):
     def test_4words(self):
         text_input = 'My name is Anna'
-        sc = StringChallenge()
-        result = sc.get_reversed_string(text_input)
+        sc = StringChallenge(text_input)
+        result = sc.get_reversed_string()
         self.assertEqual(result, 'Anna is name My')
